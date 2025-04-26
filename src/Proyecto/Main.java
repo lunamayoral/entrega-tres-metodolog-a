9667777c8cef47d8f7f7fb8d1f6a819f19f86d9c
irenemartingarcia <irene.martingarcia@edu.uah.es>
2025-04-26 20:03:38 +0200

    ejerciciocambio

--- src/Proyecto/Main.java
+++ src/Proyecto/Main.java
@@ -1,11 +1,9 @@
 package Proyecto;
 import com.google.gson.Gson;
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 public class Main {
