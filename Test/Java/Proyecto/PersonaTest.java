ca8954041adcf4c255448057d739362c0f744a85
lunamayoral <lunabookblog@gmail.com>
2025-05-01 14:03:02 +0200

    test persona

---
+++ Test/Java/Proyecto/PersonaTest.java
@@ -0,0 +1,34 @@
+package Proyecto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonaTest {
+
+    @Test
+    void getNombre() {
+        Persona personaprueba = new Persona("Luna", 22);
+        assertEquals("Luna", personaprueba.getNombre());
+    }
+
+    @Test
+    void setNombre() {
+        Persona personaprueba = new Persona("Luna", 22);
+        personaprueba.setNombre("Luni");
+        assertEquals("Luni", personaprueba.getNombre());
+    }
+
+    @Test
+    void getEdad() {
+        Persona personaprueba = new Persona("Luna", 22);
+        assertEquals(22, personaprueba.getEdad());
+    }
+
+    @Test
+    void setEdad() {
+        Persona personaprueba = new Persona("Luna", 22);
+        personaprueba.setEdad(23);
+        assertEquals(23, personaprueba.getEdad());
+    }
+}
